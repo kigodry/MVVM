19abb3c3c63ae9a13bca623fcebc9bda6a823610
Ivanov Zakhar <112906587+kigodry@users.noreply.github.com>
2023-05-19 00:02:50 +0300

    1

---
+++ MVVM/TextEditor/Commands/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Windows.Input;
+
+namespace TextEditor
+{
+    public class RelayCommand : ICommand
+    {
+        private Action _execute;
+        private Func<bool> _canExecute;
+
+        public RelayCommand(Action execute) : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ MVVM/TextEditor/ViewModels/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Input;
+
+namespace TextEditor
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    OnPropertyChanged(nameof(Text));
+                }
+            }
+        }
+
+        public ICommand NewCommand { get; private set; }
+        public ICommand OpenCommand { get; private set; }
+        public ICommand SaveCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            NewCommand = new RelayCommand(New);
+            OpenCommand = new RelayCommand(Open);
+            SaveCommand = new RelayCommand(Save);
+        }
+
+        private void New()
+        {
+            Text = string.Empty;
+        }
+
+        private void Open()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filename = openFileDialog.FileName;
+                Text = File.ReadAllText(filename);
+            }
+        }
+
+        private void Save()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filename = saveFileDialog.FileName;
+                File.WriteAllText(filename, Text);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
